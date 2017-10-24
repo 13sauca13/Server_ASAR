8676a214069f9216b496d81efde30da0be4f92f2
13sauca13 <33026257+13sauca13@users.noreply.github.com>
2017-10-24 13:46:20 +0200

    Update 5.2. Creacion multiples usuarios.ps1
    Aclaración CreacionUsuario.ps1

--- 5.2. Creacion multiples usuarios.ps1
+++ 5.2. Creacion multiples usuarios.ps1
@@ -1,7 +1,7 @@
 ﻿    #Habilita la ejcución de scripts no firmados
 Set-ExecutionPolicy RemoteSigned
     
-    #Crea todos los usuarios del CSV con el .ps1 de CreeacionUsuario
-    #HAY QUE REVISAR LA UBICACION DEL CSV Y DEL PS1!!!!
+    #Crea todos los usuarios del CSV con el .ps1 de 5.2.2.CreeacionUsuario
+    #HAY QUE REVISAR LA UBICACION DEL CSV Y RENOMBRAR Y UBICAR EL PS1!!!!
 Import-Csv .\Desktop\UsuariosDominio.csv | ForEach-Object {
     .\Documents\CreacionUsuario.ps1 -Nombre $_.Nombre -Apellido1 $_.Apellido1 -Apellido2 $_.Apellido2 -Logon $_.Login}
